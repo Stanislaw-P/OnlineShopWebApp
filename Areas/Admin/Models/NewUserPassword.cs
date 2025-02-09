4d663c953107c3465686e0c19f23ec17e0e46a75
Stas Persaev <persaev10@mail.ru>
2025-02-09 22:09:30 +0300

    Добавил действие редактирования пароля у выбранного пользователя для администратора

---
+++ Areas/Admin/Models/NewUserPassword.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Areas.Admin.Models
+{
+	public class NewUserPassword
+	{
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Обязательное поле!")]
+		[StringLength(100, MinimumLength = 6, ErrorMessage = "Пароль должен содержать минимум 6 символов!")]
+		public string Password { get; set; }
+
+        [Required(ErrorMessage = "Обязательное поле!")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают!")]
+        public string ConfirmPassword { get; set; }
+    }
+}
