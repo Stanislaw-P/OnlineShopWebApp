09a082588175138576c9e492bf7fc95988ebd60c
Stas Persaev <persaev10@mail.ru>
2025-03-02 13:08:02 +0300

    Решил задачу 2 (Добавил избранные продукты на хранение БД)

---
+++ Controllers/FavoriteController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Db;
+using OnlineShopWebApp.Helpers;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class FavoriteController : Controller
+	{
+		readonly IFavoriteRepository favoritesRepository;
+		readonly IProductsRepository productsRepository;
+
+		public FavoriteController(IFavoriteRepository favoritesRepository, IProductsRepository productsRepository)
+		{
+			this.favoritesRepository = favoritesRepository;
+			this.productsRepository = productsRepository;
+		}
+
+		public IActionResult Index()
+		{
+			var products = favoritesRepository.GetAll(Constants.UserId);
+			return View(MappingHelper.ToProductViewModels(products));
+		}
+
+		public IActionResult Add(Guid productId)
+		{
+			var product = productsRepository.TryGetById(productId);
+			favoritesRepository.Add(product, Constants.UserId);
+			return RedirectToAction(nameof(Index), "Home");
+		}
+
+		public IActionResult RemoveProduct(Guid productId)
+		{
+			var product = productsRepository.TryGetById(productId);
+			favoritesRepository.Remove(product, Constants.UserId);
+			return RedirectToAction(nameof(Index));
+		}
+	}
+}
--- Models/Wishlist.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OnlineShopWebApp.Models
-{
-	public class Wishlist
-	{
-        public Guid ID { get; set; }
-        public string UserId { get; set; }
-        public List<ProductViewModel> Items { get; set; }
-    }
-}
